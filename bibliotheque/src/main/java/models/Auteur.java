d819c166b2cdd2ab195bd01af3c4a51b61f3d0e6
MickaLillois <mickaelcarceles@gmail.com>
2021-11-29 17:00:44 +0100

    Push

--- bibliotheque/src/main/java/models/Auteur.java
+++ bibliotheque/src/main/java/models/Auteur.java
@@ -1,5 +1,20 @@
 package models;
 
-public class Auteur {
+import java.time.LocalDateTime;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Auteur {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String prenom;
+	private String nom;
+	private LocalDateTime dateNaissance;
+	private int idBibliotheque;
+	
+	protected Auteur() {}
 }
--- bibliotheque/src/main/java/models/Lecteur.java
+++ bibliotheque/src/main/java/models/Lecteur.java
@@ -10,14 +10,64 @@ import javax.persistence.Id;
 @Entity
 public class Lecteur {
 
-  @Id
-  @GeneratedValue(strategy=GenerationType.IDENTITY)
-  private int id;
-  private String prenom;
-  private String nom;
-  private LocalDateTime dateNaissance;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String prenom;
+	private String nom;
+	private LocalDateTime dateNaissance;
+	private int idBibliotheque;
+	
+	protected Lecteur() {}
 
-  protected Lecteur() {}
+	public Lecteur(String prenom, String nom, LocalDateTime dateNaissance, int idBibliotheque) {
+		super();
+		this.prenom = prenom;
+		this.nom = nom;
+		this.dateNaissance = dateNaissance;
+		this.idBibliotheque = idBibliotheque;
+	}
 
-  
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public LocalDateTime getDateNaissance() {
+		return dateNaissance;
+	}
+
+	public void setDateNaissance(LocalDateTime dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+	
+	public int getIdBibliotheque() {
+		return idBibliotheque;
+	}
+
+	
+	public void setIdBibliotheque(int idBibliotheque) {
+		this.idBibliotheque = idBibliotheque;
+	}
+	
 }
