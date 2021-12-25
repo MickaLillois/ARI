4e334b2ecebcfc457047b06f012806db15f4ff97
MickaLillois <mickaelcarceles@gmail.com>
2021-12-25 20:24:44 +0100

    Tests pour faire fonctionner H2

--- bibliotheque/src/main/java/com/security/WebSecurityConfig.java
+++ bibliotheque/src/main/java/com/security/WebSecurityConfig.java
@@ -2,7 +2,9 @@ package com.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -15,19 +17,15 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.anyRequest().permitAll()
-				//.antMatchers("/", "/home").permitAll()
-				//.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/login")
-				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
+		http.authorizeRequests()
+        .antMatchers("/h2/**").hasRole("ADMIN")//allow h2 console access to admins only
+        .anyRequest().authenticated()//all other urls can be access by any authenticated role
+        .and().formLogin()//enable form login instead of basic login
+        .and().csrf().ignoringAntMatchers("/h2-console/**")//don't apply CSRF protection to /h2-console
+        .and().headers().frameOptions().sameOrigin();//allow use of frame to same origin urls
 	}
+
+
 
 	@Bean
 	@Override
@@ -36,7 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			 User.withDefaultPasswordEncoder()
 				.username("user")
 				.password("password")
-				.roles("USER")
+				.roles("ROLE_USER")
 				.build();
 
 		return new InMemoryUserDetailsManager(user);
